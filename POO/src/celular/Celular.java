24627568898442f759f34b88f00c4479a2566d84
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-08-09 18:18:39 -0300

    Add files via upload

---
+++ POO/src/celular/Celular.java
@@ -0,0 +1,9 @@
+package celular;
+
+public class Celular {
+	// criaçãp de atributos - caracteristicas
+	public String nome;
+	public String sistemaOperacional;
+	public int armazenamento;
+	public double tamanho;
+}

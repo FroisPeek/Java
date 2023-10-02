0c4af79cdf364bb95945d1a7c0bb15dc01251077
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-10-02 16:22:01 -0300

    Delete POO/src directory

--- POO/src/celular/Celular.java
+++
@@ -1,9 +0,0 @@
-package celular;
-
-public class Celular {
-	// criação de atributos - caracteristicas
-	public String nome;
-	public String sistemaOperacional;
-	public int armazenamento;
-	public double tamanho;
-}
